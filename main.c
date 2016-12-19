cd8477dcde7fc650b6ebbab3632762168e99b878
Leonelney <leonelolivares80@gmail.com>
2016-12-19 14:31:33 -0600

    second

--- main.c
+++ main.c
@@ -1,177 +1,145 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int a;
 
-void
-print_row (int M, int j, int matriz[M][M])
-{
-  int i = 0;
+void ordenar (int N, float *x);
 
-  for (i = 0; i < M; i++)
-    {
-      printf ("%d ", matriz[i][j]);
-    }
+float
+modulo (float x, int b)
+{
+  x = x / b;
+  x = x - (int) x;
+
+  return x;
 }
 
-void
-print_mat (int M, int matriz[M][M])
+float
+promediar_columnas_pares (int N, float mat[N][N])
 {
 
-  int j = 0;
+  int i;
+  int j;
+  int b = 0;
+  float r = 0;
 
-  printf ("\n");
-
-  for (j = 0; j < M; j++)
+  for (i = 0; i < N; i = i + 1)
     {
-      print_row (M, j, matriz);
-      printf ("\n");
+      if (modulo (i, 2) == 0)
+	{
+	  for (j = 0; j < N; j = j + 1)
+	    {
+	      r = r + mat[i][j];
+	      b = b + 1;
+
+	    }
+	}
     }
-}
 
-int
-calc_det2x2 (int matrix[2][2])
-{
-
-  int r = 0;
-
-  r = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
+  r = r / b;
 
   return r;
 }
 
-
-int
-calc_det3x3 (int matrix[3][3])
+float
+promediar_diagonales (int N, float mat[N][N])
 {
 
-  int i = 0;
-  int r = 0;
-  int matrix2[2][2];
+  int i;
+  int j;
+  int b = 0;
+  float r = 0;
 
-  for (; i < 3; i++)
-    {
-      matrix2[0][0] = matrix[1][1];
-      matrix2[1][0] = matrix[1][2];
-      matrix2[0][1] = matrix[1][1];
-
-      r += pow(-1, i % 2) * matrix[0][i] * calc_det2x2 (matrix2);
-      /*
-      printf("\nr += pow(-1, %d mod 2) * matrix[0][%d] * calc_det2x2(matrix2)", i, i);
-      printf("\nr += %d * %d * %d", (int) pow(-1, i % 2), matrix[0][i], calc_det2x2(matrix2));
-      printf("\nr = %d", r);
-      */
-    }
-}
-
-void
-reduce_mat (int M, int mat[M][M], int mat3[3][3])
-{
-
-  mat3[0][0] = mat[0][M - 2];
-  mat3[1][0] = mat[1][M - 2];
-  mat3[2][0] = mat[2][M - 2];
-
-  mat3[0][1] = mat[0][M - 1];
-  mat3[1][1] = mat[1][M - 1];
-  mat3[2][1] = mat[2][M - 1];
-
-  mat3[0][2] = mat[0][M];
-  mat3[1][2] = mat[1][M];
-  mat3[2][2] = mat[2][M];
-}
-
-void
-fill_mat (int M, int mat[M][M], int *datos)
-{
-
-  int i = 0;
-  int j = 0;
-
-  for (j = 0; j < M; j++)
-    {
-      for (i = 0; i < M; i++)
-    	{
-    	  mat[i][j] = *(datos + a);
-    	  a++;
-    	}
-    }
-
-  print_mat (M, mat);
-}
-
-void
-operate (int N, int M, int det[N], int *datos)
-{
-
-  int matrix[M][M];
-  int mat3[3][3];
-  int i = 0;
-  
   for (i = 0; i < N; i++)
     {
-      fill_mat(M, matrix, datos);
-      reduce_mat (M, matrix, mat3);
-      det[i] = calc_det3x3 (mat3);
-      printf("%d\n", det[i]);
+      for (j = 0; j < N; j++)
+	{
+	  if (i == j)
+	    {
+	      r = r + mat[i][j];
+	      b = b + 1;
+	    }
+	}
     }
+
+  r = r / b;
+
+  return r;
 }
 
 void
-order(int N, int det[N]) {
-  int a = 0;
-  int i = 0;
-  int j = 0;
-
-  for (; i < N; i++) {
-    for (j = i + 1; j < N; j++) {
-      if (det[i] < det[j]) {
-        a = det[i];
-        det[i] = det[j];
-        det[j] = a;
-      }
-    }
-  }
-}
-
-void
-examen (int N, int M, int *datos)
+llenar_mat (int N, float mat[5][5], float *datos)
 {
 
   int i = 0;
+  int j = 0;
+  int b = N * N * a;
 
-  if (M >= 3)
+  for (i = 0; i < N; i = i + 1)
     {
-      int det[N];
+      for (j = 0; j < N; j = j + 1)
+	{
+	  mat[i][j] = *(datos + b);
+	}
+    }
+}
 
-      operate(N, M, det, datos);
-      order(N, det);
+void
+extra (float *datos, int N)
+{
+  int i;
+  a = 0;
+  float *x = 0;
+  float mat[N][N];
 
-      for (; i < N; i++)
-      {
-        printf("\n%d", det[i]);
-      }
-      printf("\n");
+  for (i = 0; i < 8; i = i + 1)
+    {
+      *(datos + i) = i;
+    }
+
+  for (i = 0; i < N; i = i + 1)
+    {
+      llenar_mat (N, mat, datos);
+      *(x + i) =
+	promediar_columnas_pares (N, mat) - promediar_diagonales (N, mat);
+      a = a + 1;
+    }
+
+  ordenar (N, x);
+
+  for (i = 0; i < N; i = i + 1)
+    {
+      printf ("%f", *(x + i));
+    }
+}
+
+void
+ordenar (int N, float *x)
+{
+
+  int i;
+  int j;
+  float aux;
+
+  for (i = 0; i < N; i = i + 1)
+    {
+      for (j = 0; j < N; j = j + 1)
+	{
+	  if (*(x + i) < *(x + j))
+	    {
+	      aux = *(x + j);
+	      *(x + j) = *(x + i);
+	      *(x + i) = aux;
+	    }
+	}
     }
 }
 
 int
 main ()
 {
-  a = 0;
-  int N = 3;
-  int M = 4;
-  int i = M * M * N;
-  int det[M];
-  int *datos = (int *) malloc (i * sizeof (int));
-
-  for (i = 0; i < 48; i++)
-    {
-      *(datos + i) = -i;
-    }
-  
-  examen(N, M, datos);
+  int N = 2;
+  float *datos = (float *) malloc (N * N * N * sizeof (float));
+  extra (datos, N);
   free (datos);
-  //print_mat(matrix, 3);
 }
